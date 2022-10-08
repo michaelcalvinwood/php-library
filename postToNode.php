98e40a0f226c8ac0ce9061e6a6593ac0b9f986b7
Michael Wood <michaelwood33311@icloud.com>
2022-10-08 10:30:18 -0400

    added function to post to nodejs service using json

---
+++ postToNode.php
@@ -0,0 +1,27 @@
+<?php
+
+function postToNode($data, $url, $port = 80, $debug = false) {
+    $payload = json_encode($data);
+ 
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    curl_setopt($ch, CURLOPT_PORT, $port);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($payload))
+    );
+    
+    $result = curl_exec($ch);
+    
+    if ($result === false && $debug) echo curl_error($ch);
+
+    curl_close($ch);
+
+    return $result;
+}
