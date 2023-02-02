0b97c1866ab60a370f1a9c17c99940ac00376cca
Michael Wood <michaelwood33311@icloud.com>
2023-02-02 09:35:03 -0500

    /jwt/: refactored to retrieve jwt token for authorization header

--- jwt/authenticate.php
+++ jwt/authenticate.php
@@ -36,11 +36,18 @@ echo $jwt;
     RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
 */
 
-function checkHeaderForAuthorization() {
+function retrieveJwtFromHeader() {
     if (! preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
         header('HTTP/1.0 400 Bad Request');
         echo 'Token not found in request';
         exit;
     }
-    return $_SERVER['HTTP_AUTHORIZATION'];
+    $jwt = $matches[1];
+    if (! $jwt) {
+        // No token was able to be extracted from the authorization header
+        header('HTTP/1.0 400 Bad Request');
+        exit;
+    }
+    return $jwt;
 }
+
